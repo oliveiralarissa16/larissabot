7b455713136c7e0080707edfe84c9a4280b63928
oliveiralarissa16 <larissa16ribeiro46@gmail.com>
2016-12-07 16:26:51 -0200

    Update bot_telegram.sql

--- bot_telegram.sql
+++ bot_telegram.sql
@@ -2,10 +2,26 @@ create database Bot_telegram;
 
 use Bot_Telegram;
 
-create table tbbotresponse(
-id int auto_increment primary key ,
-update_id char not null unique key,
-comando varchar(100) not null,
-resposta varchar(100) not null,
-data_resposta timestamp not null default current_timestamp
+create table comando(
+idComando int primary key AUTO_INCREMENT,
+texto varchar(20) not null
 );
+
+insert into comando
+ (texto)
+ values
+  ('/megaSena'),
+  ('/photo');
+
+
+create table tbbotresponse(
+id int auto_increment primary key,
+update_id float not null unique key,
+comando int,
+resposta varchar(100) not null,
+data_resposta timestamp not null default current_timestamp,
+foreign key(comando) references comando(idComando)
+);
+
+
+SELECT idComando, id, texto FROM comando AS c LEFT JOIN tbbotresponse AS t on c.idComando = t.id;
