3062cbee8d1510ca4a9e971d8ac4d4e6e07ead90
oliveiralarissa16 <larissa16ribeiro46@gmail.com>
2016-12-08 11:22:18 -0200

    Update index.php

--- index.php
+++ index.php
@@ -33,15 +33,15 @@
 
                 $arrUpdateId = explode(',', $str);
                 if (!in_array($updateId, $arrUpdateId)) {
-                    # Gera os 6 n�meros
+                    # Gera os 6 números
                     for ($i = 1; $i <= 6; $i++) {
                         $n[] = str_pad(rand(1, 60), 2, '0', STR_PAD_LEFT);
                     }
 
-                    # Ordena os n�meros
+                    # Ordena os números
                     sort($n);
 
-                    # Exibe os n�meros
+                    # Exibe os números
                     $numerosmega = implode(' - ', $n);
 
                     $texto = urlencode($numerosmega);
