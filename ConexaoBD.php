1d10f2d06caab5ff7557729395b1b360b5a9e5c8
oliveiralarissa16 <larissa16ribeiro46@gmail.com>
2016-12-08 13:34:38 -0200

    Update ConexaoBD.php

--- ConexaoBD.php
+++ ConexaoBD.php
@@ -1,19 +1,40 @@
 <?php
-
 class ConexaoBD {
-    public function conectarbanco(){
-        $conexao = new PDO("mysql:host=localhost;dbname=Bot_telegram","root","");
+    public static function conectarbanco(){
+        $conexao = new PDO("mysql:host=localhost;dbname=bot_telegram","root","");
         return $conexao;
         
     }
-    public function inserirdados($updateid,$comando,$resposta){
-        $sql = "insert into tbbotresposta(update_id,comando,resposta) values (?,?,?)";
+    private static function verificarComando($texto) {
+
+        $verificarComando = "SELECT idComando FROM comando WHERE texto = ?";
+
+        $idComando = "";
+        try {
+            $rs = self::conectarbanco()->prepare($verificarComando);
+            $rs->bindParam(1, $texto);
+            $rs->execute();
+
+            while ($row = $rs->fetch(PDO::FETCH_OBJ)) {
+                $idComando = $row->idComando;
+            }
+
+            return $idComando;
+        } catch (PDOException $exc) {
+            echo $exc->getMessage();
+        }
+    }
+     public function inserirdados($updateid,$comando,$resposta){
+         $sql = "insert into tbbotresponse(update_id,comando,resposta) values (?,?,?)";
+         
+         $id_comando = self::verificarComando($comando);
         
-        $prepararcomando = self::conectarbanco()->prepare($sql);
+         $prepararcomando = self::conectarbanco()->prepare($sql);
          $prepararcomando->bindParam(1, $updateid);
-         $prepararcomando->bindParam(2, $comando);
+         $prepararcomando->bindParam(2, $id_comando);
          $prepararcomando->bindParam(3, $resposta);
          
          $prepararcomando->execute();
     }
 }
+
